10eb4d0bcbdfa32c53db3b7dfc2efe4b62aa4a76
Robert Barrios <ryba.9125@gmail.com>
2021-12-12 00:48:08 -0500

    making the others methods of a CRUD

--- conexion.py
+++ conexion.py
@@ -8,11 +8,11 @@ class Contactos:
         return conexion
 
     def leer_contactos(self):
-        conexion = self.iniciar_conexion8
+        conexion = self.iniciar_conexion()
         cursor = conexion.cursor()
         sentenciaSQL = "SELECT * FROM contactos"
         cursor.execute(sentenciaSQL)
-        print(cursor.fetchall())
+        return cursor.fetchall()
         # db_name = 'sistema.db'
         # query = "SELECT * FROM contactos"
         # with sqlite3.connect(db_name) as conn:
@@ -21,5 +21,26 @@ class Contactos:
         #     conn.commit()
         # print(result)
     
+    def crear_contacto(self, datos_contacto):
+        conexion = self.iniciar_conexion()
+        cursor = conexion.cursor()
+        sentenciaSQL = "INSERT INTO contactos(nombre, correo) VALUES(?, ?)"
+        cursor.execute(sentenciaSQL, datos_contacto)
+        conexion.commit()
+        conexion.close()
     
+    def borrar_contacto(self, id_contacto):
+        conexion = self.iniciar_conexion()
+        cursor = conexion.cursor()
+        sentenciaSQL = "DELETE FROM contactos WHERE id=(?)"
+        cursor.execute(sentenciaSQL, [id_contacto])
+        conexion.commit()
+        conexion.close()
 
+    def modificar_contacto(self, datos_contacto):
+        conexion = self.iniciar_conexion()
+        cursor = conexion.cursor()
+        sentenciaSQL = "UPDATE contactos SET nombre=?, correo = ? WHERE id=?"
+        cursor.execute(sentenciaSQL, datos_contacto)
+        conexion.commit()
+        conexion.close()
--- crud.py
+++ crud.py
@@ -6,26 +6,65 @@ from conexion import *
 
 def agregar():
     print("Hola, soy la acción de agregar")
+    nombre = ventana.txtNombre.text() # es como el get de tkinter
+    correo = ventana.txtCorreo.text()
+    print(nombre, correo)
     obj_contactos = Contactos()
-    obj_contactos.iniciar_conexion()
+    contactos = obj_contactos.crear_contacto((nombre, correo))
+    consultar()
 
 def modificar():
     print("Hola, soy la acción de modificar")
+    id = ventana.txtId.text()
+    nombre = ventana.txtNombre.text()
+    correo = ventana.txtCorreo.text()
+
+    obj_contactos = Contactos()
+    contactos = obj_contactos.modificar_contacto((nombre, correo, id))
+    consultar()
 
 def eliminar():
     print("Hola, soy la acción de eliminar")
+    id = ventana.txtId.text()
+    obj_contactos = Contactos()
+    obj_contactos.borrar_contacto(id)
+    consultar()
 
 def cancelar():
     print("Hola, soy la acción de cancelar")
+
+def consultar():
+    ventana.tableContactos.setRowCount(0) # limpiar la tabla 
+    indice_control = 0
+    obj_contactos = Contactos()
+    contactos = obj_contactos.leer_contactos()
+    for contacto in contactos:
+        ventana.tableContactos.setRowCount(indice_control + 1)
+        ventana.tableContactos.setItem(indice_control, 0, QTableWidgetItem(str(contacto[0])))
+        ventana.tableContactos.setItem(indice_control, 1, QTableWidgetItem(str(contacto[1])))
+        ventana.tableContactos.setItem(indice_control, 2, QTableWidgetItem(str(contacto[2])))
+        indice_control += 1
+
+def seleccionar():
+    id = ventana.tableContactos.selectedIndexes()[0].data()
+    nombre = ventana.tableContactos.selectedIndexes()[1].data()
+    correo = ventana.tableContactos.selectedIndexes()[2].data()
+    print(id, nombre, correo)
+    ventana.txtId.setText(id)
+    ventana.txtNombre.setText(nombre)
+    ventana.txtCorreo.setText(correo)
 
 aplicacion = QtWidgets.QApplication([])
 
 ventana = uic.loadUi("ventana.ui")
 ventana.show()
+consultar()
 #Poner nombre a los campos
 ventana.tableContactos.setHorizontalHeaderLabels(['ID', 'Nombre', 'Correo'])
 ventana.tableContactos.setEditTriggers(QTableWidget.NoEditTriggers)
 ventana.tableContactos.setSelectionBehavior(QTableWidget.SelectRows)
+
+ventana.tableContactos.cellClicked.connect(seleccionar)
 
 ventana.btnAgregar.clicked.connect(agregar)
 ventana.btnModificar.clicked.connect(modificar)
